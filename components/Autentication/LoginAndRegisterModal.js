38675b37d80a5fe97a4380b1ece21535a65f4fe9
johan <jgamboa@iansresearch.com>
2022-05-14 13:46:10 -0500

    update

--- components/Autentication/LoginAndRegisterModal.js
+++ components/Autentication/LoginAndRegisterModal.js
@@ -37,7 +37,7 @@ export default function LoginAndRegisterModal({ isOpen, setIsOpen, formToRender,
 			<Transition appear show={isOpen} as={Fragment}>
 				<Dialog as='div' className='fixed inset-0 z-10 overflow-y-auto' onClose={closeModal}>
 					<div className='min-h-screen px-4 text-center'>
-						<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0' closeModal={closeModal}>
+						<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
 							<Dialog.Overlay className='fixed inset-0 opacity-90 bg-black' />
 						</Transition.Child>
 
