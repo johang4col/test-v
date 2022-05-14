d6600a6970bcc668cb8c2ce41a261abfc8e892f8
johan <jgamboa@iansresearch.com>
2022-05-14 13:35:32 -0500

    on

--- components/Autentication/LoginAndRegisterModal.js
+++ components/Autentication/LoginAndRegisterModal.js
@@ -34,12 +34,12 @@ export default function LoginAndRegisterModal({ isOpen, setIsOpen, formToRender,
 
 	return (
 		<>
-			<Transition appear show={isOpen} as={Fragment}>
-				<Dialog as='div' className='fixed inset-0 z-10 overflow-y-auto' onClose={closeModal}>
+			<Transition appear show={isOpen} as={Fragment} onClose={closeModal}>
+				<Dialog as='div' className='fixed inset-0 z-10 overflow-y-auto'>
 					<div className='min-h-screen px-4 text-center'>
-						{/* <Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
+						<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0' enterTo='opacity-100' leave='ease-in duration-200' leaveFrom='opacity-100' leaveTo='opacity-0'>
 							<Dialog.Overlay className='fixed inset-0 opacity-90 bg-black' />
-						</Transition.Child> */}
+						</Transition.Child>
 
 						{/* This element is to trick the browser into centering the modal contents. */}
 						<span className='inline-block h-screen align-middle' aria-hidden='true'>
