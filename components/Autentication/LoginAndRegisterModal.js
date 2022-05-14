3bafcfd8dcf1b19fcd80680292453b5385042b1c
johan <jgamboa@iansresearch.com>
2022-05-14 14:05:34 -0500

    Update LoginAndRegisterModal.js

--- components/Autentication/LoginAndRegisterModal.js
+++ components/Autentication/LoginAndRegisterModal.js
@@ -46,7 +46,7 @@ export default function LoginAndRegisterModal({ isOpen, setIsOpen, formToRender,
 						<span className='inline-block h-screen align-middle' aria-hidden='true'>
 							&#8203;
 						</span>
-						<Transition.Child as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'>
+						<Dialog.Panel as={Fragment} enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95'>
 							<div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl'>
 								<Dialog.Title as='h3' className='text-lg font-medium leading-6 text-gray-900'></Dialog.Title>
 
@@ -71,7 +71,7 @@ export default function LoginAndRegisterModal({ isOpen, setIsOpen, formToRender,
 									</button>
 								</div>
 							</div>
-						</Transition.Child>
+						</Dialog.Panel>
 					</div>
 				</Dialog>
 			</Transition>
