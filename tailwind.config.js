c217df473742c428e1c26e6ba341c25961485170
johan <jgamboa@iansresearch.com>
2022-05-14 13:26:56 -0500

    src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	mode: 'jit',
-	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-	darkMode: 'class', // or 'media' or 'class'
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'class', // false or 'media' or 'class'
 	theme: {
 		colors: {
 			transparent: 'transparent',
